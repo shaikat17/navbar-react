0912ae16d201220eedec10eb545f9925e7a2fea0
shaikat17 <shaikatpal56@gmail.com>
2022-10-28 00:29:24 +0600

    working on Links and Icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,18 +15,14 @@ const Navbar = () => {
         </div>
         <div className="links-container show-container">
           <ul className="links">
-            <li>
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a href="#">About</a>
-            </li>
-            <li>
-              <a href="#">Contact</a>
-            </li>
-            <li>
-              <a href="#">Products</a>
-            </li>
+            {links.map((link) => {
+              const {id, url, text} = link
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              )
+            })}
           </ul>
         </div>
         <ul className="social-icons">
