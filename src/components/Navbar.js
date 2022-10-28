e2a1caf04b20d0525d84f27c96ff00ff4eaf3870
shaikat17 <shaikatpal56@gmail.com>
2022-10-29 00:14:04 +0600

    toggle functionality in dynamic way

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,19 @@ import logo from "../logo.png";
 
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false)
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+  // const toggleLinks = () => {
+  //   setShowLinks(!showLinks);
+  // };
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = '0px';
+    }
+  }, [showLinks]);
   return (
     <nav>
       <div className="nav-center">
@@ -31,8 +44,22 @@ const Navbar = () => {
         } */}
 
         {/* Second Way */}
-        <div className={`${showLinks ? "links-container show-container" : "links-container"}`}>
+        {/* <div className={`${showLinks ? "links-container show-container" : "links-container"}`}>
           <ul className="links">
+            {links.map((link) => {
+              const {id, url, text} = link
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              )
+            })}
+          </ul>
+        </div> */}
+
+        {/* Third Way with dynamic */}
+        <div className="links-container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef} >
             {links.map((link) => {
               const {id, url, text} = link
               return (
